31e8bf8464b15bb56134e781b14d026bbb003e9e
Belen <>
2018-10-05 00:19:16 +0200

    check authenticated

--- routes/destiny.js
+++ routes/destiny.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const destinyController = require('../controllers/destiny.controller');
+const auth = require('../middlewares/auth.middleware');
 
-router.get('/', destinyController.list); // registrado
-router.post('/', destinyController.create); // registrado
-router.get('/:id', destinyController.get); // registrado
-router.delete('/:id', destinyController.delete) //registrado
+router.get('/', auth.isAuthenticated, destinyController.list); 
+router.post('/', auth.isAuthenticated, destinyController.create); 
+router.get('/:id', auth.isAuthenticated, destinyController.get); 
+router.delete('/:id', auth.isAuthenticated, destinyController.delete) 
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/users.controller');
 const userDestController = require('../controllers/userDestiny.controller');
+const auth = require('../middlewares/auth.middleware');
 
-router.get('/', userController.list); //registrado    //solo admin
-router.post('/', userController.create);
-router.delete('/:id', userController.delete); //regsitrado //solo admin
+router.get('/', auth.isAuthenticated, userController.list);   //solo admin
+router.post('/', auth.isAuthenticated, userController.create);
+router.delete('/:id', auth.isAuthenticated, userController.delete); //solo admin
 
-router.get('/:id/myDestinies', userDestController.list); //registrado //solo user (y admin?)
-router.post('/:id/myDestinies', userDestController.create); //registrado //solo user (y admin?)
+router.get('/:id/myDestinies', auth.isAuthenticated, userDestController.list); //solo user (y admin?)
+router.post('/:id/myDestinies', auth.isAuthenticated, userDestController.create);  //solo user (y admin?)
 
 module.exports = router;
